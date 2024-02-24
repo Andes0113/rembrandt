3c3d72a3434b3e1208a22e8904648fe560442c45
Sherry Long <sherrylong@wirelessprv-10-194-56-112.near.illinois.edu>
2024-02-24 00:43:38 -0600

    Created generate API route

--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,24 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from client.generate import generate_image
 from client.describe import describe_image
+from pydantic import BaseModel
 
 
 load_dotenv()
 
+class Item(BaseModel):
+    url: str
+    prompt: str
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
-    url = generate()
+    url = generate_image()
     return {"success": True, "url": url}
 
 @app.post('/generate')
-async def generate_image(url, prompt):
-    qualities = describe_image(url)
-    photo_url = generate_image(prompt, qualities)
+async def generate(query: Item):
+    qualities = describe_image(query.url)
+    photo_url = generate_image(query.prompt, qualities)
     return {"success": True, "url": photo_url}
