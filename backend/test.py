f5900f53f6461699a318759ed803a8a0ff91820f
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2024-02-23 22:57:39 -0600

    .env

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,16 @@
 from openai import OpenAI
-client = OpenAI(api_key='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')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+prompt_context="high quality tech company logo with the following subject:"
 
 response = client.images.generate(
   model="dall-e-3",
-  prompt="a white siamese cat",
+  prompt=prompt_context + "futuristic, blue and pink coloring, a cat",
   size="1024x1024",
   quality="standard",
   n=1,
