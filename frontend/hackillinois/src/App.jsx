dde5435745785c8da31b758b78020200283b265c
Sherry Long <sherrylong@wirelessprv-10-194-56-112.near.illinois.edu>
2024-02-24 18:03:37 -0600

    Imported logo

--- frontend/hackillinois/src/App.jsx
+++ frontend/hackillinois/src/App.jsx
@@ -44,9 +44,9 @@ function App() {
   return (
     <div id="container">
       <div id = "header">
-        <img src={"https://i.pinimg.com/originals/82/c6/5b/82c65b9bb0a75026fc4c82a438b4cc9b.jpg"} height="100px"/>
+        <img src={"logo.png"} height="100px"/>
       </div>
-      
+
       <div id = "content">
         <div id="inputs">
           <div id = "inputUrl">
